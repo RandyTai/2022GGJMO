d5bbcc2d630ec0c19908b2b99eb7a9f57fcdded3
TongIp <56107006+TongIp@users.noreply.github.com>
2022-01-29 21:00:26 +0800

    Update fight.gd

--- tong/fight.gd
+++ tong/fight.gd
@@ -7,6 +7,9 @@ extends Node2D
 var speed = 400
 var flyx = 0
 var flyy = 0
+var flyx2 = 0
+var flyy2= 0
+
 onready var player1 = $Player
 onready var player2 = $Player2
 onready var runsound =$run
@@ -78,25 +81,54 @@ func _process(delta):
 	#	flyy = 0
 	player1.position.x += flyx * speed *delta *2
 	player1.position.y += flyy * speed *delta	*2	
+	
 	player1.position.x = clamp(player1.position.x,30,990)
 	player1.position.y = clamp(player1.position.y,30,570)
 	
 	if Input.is_action_pressed("P2_move_left"):
-		runsound.play()
+		#runsound.play()
 		if tey2 == 30 || tey2 == 570:
+			flyx2 = 0
+			flyy2 = 0
 			player2.position.x -= speed * delta
 		
 	if Input.is_action_pressed("P2_move_right"):
 		if tey2 == 30 || tey2 == 570:
+			flyx2 = 0
+			flyy2 = 0
 			player2.position.x += speed * delta
 			
 	if Input.is_action_pressed("P2_move_up"):
 		if tex2 == 30 || tex2 == 990:
+			flyx2 = 0
+			flyy2 = 0
 			player2.position.y -= speed * delta
 		
-	if Input.is_action_pressed("P2_move_down"):
+	if Input.is_action_pressed("P1_move_down"):
 		if tex2 == 30 || tex2 == 990:
+			flyx2 = 0
+			flyy2 = 0
 			player2.position.y += speed * delta	
+			
+	if Input.is_action_pressed("P2_jump"):
+		if tex2 == 30 || tex2 == 990:
+			if tex2 == 30:	
+				flyx2 = 1
+			if tex2 == 990:
+				flyx2 = -1
+				
+			
+		if tey2 == 30 || tey2 == 570:
+			if tey2 == 30:
+				flyy2 = 1
+			if tey2 == 570:
+				flyy2 = - 1
+				
+	#if tex1 == 30 || tex1 == 990 || tey1 == 30 || tey1 == 570:
+	#	flyx = 0
+	#	flyy = 0
+	player2.position.x += flyx2 * speed *delta *2
+	player2.position.y += flyy2 * speed *delta	*2	
 			
 	player2.position.x = clamp(player2.position.x,30,990)
 	player2.position.y = clamp(player2.position.y,30,570)
