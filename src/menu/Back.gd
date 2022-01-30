b779fdd5b053ccf7bed486a987c567cffa4ffb0f
jason489 <l.jason489@gmail.com>
2022-01-30 13:47:44 +0800

    add new image

--- src/menu/Back.gd
+++ src/menu/Back.gd
@@ -2,3 +2,4 @@ extends Button
 
 func _on_Back_button_up() -> void:
 	get_tree().quit()
+	print("back")
--- src/menu/Restart.gd
+++ src/menu/Restart.gd
@@ -4,4 +4,5 @@ extends Button
 func _on_Restart_button_up() -> void:
 	playerData.reset()
 	get_tree().paused = false
+	print("restart")
 	var _a = get_tree().reload_current_scene()
